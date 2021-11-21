63e43ce7ce096751aed66e17231693be7b8f5a87
DESKTOP-21E13UO\dimos <dmaginas@web.de>
2021-11-21 12:17:25 +0100

    Added a comment to Program.cs

--- MyCSharpConsoleApp/MyCSharpConsoleApp/Program.cs
+++ MyCSharpConsoleApp/MyCSharpConsoleApp/Program.cs
@@ -6,6 +6,7 @@ namespace MyCSharpConsoleApp
     {
         static void Main(string[] args)
         {
+            // Please change something and commit it to github
             Console.WriteLine("Hello WERBAS!");
             Console.ReadLine();
         }
